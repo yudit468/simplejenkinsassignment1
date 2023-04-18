e127fb2a4e893280c473552c4f063317186df915
Uditnarayan Yadav <uditnarayan.yadav@atos.net>
2023-04-18 15:36:04 +0530

    initial commit

---
+++ src/main/java/com/assignment/exercise1/Exercise1Application.java
@@ -0,0 +1,16 @@
+package com.assignment.exercise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise1Application.class, args);
+	}
+
+}
+
+
+
